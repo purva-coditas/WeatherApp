472f520cb5334a99dd6452a52fc33ea36eafe133
Purva Padmawar <purva.padmawar@coditas.com>
2022-04-27 15:55:32 +0530

    initial setup

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+const App = () => {
+  return <h1>Weather Forecast</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
