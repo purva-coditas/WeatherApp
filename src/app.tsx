627ba97685a8bdf3065dce2faa0b714152244093
raginimasanta <ragini.masanta@coditas.com>
2022-05-19 12:21:52 +0530

    removed duplicate files

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.scss';
-import GrabData from './component/GrabData';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import 'react-clock/dist/Clock.css';
-import Progressbar from './component/Progressbar';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.scss";
+import GrabData from "./component/GrabData";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "react-clock/dist/Clock.css";
 
 const App = () => {
   return (
     <>
       {/* <h1>Weather Forecast</h1> */}
       <GrabData />
-      <Progressbar />
     </>
   );
 };
